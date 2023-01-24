932b50f22941f6a6e2d9caa25adcee465c811abf
ali <wizard2070@gmail.com>
2023-01-24 23:24:14 +0330

    add_movie_details

--- lib/screens/movieDetail_screen.dart
+++ lib/screens/movieDetail_screen.dart
@@ -15,6 +15,35 @@ class MovieDetailScreen extends StatelessWidget {
       appBar: AppBar(
         title: Text(loadedMovie.title),
       ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
+              height: 300,
+              width: double.infinity,
+              child: Image.network(
+                loadedMovie.poster,
+                fit: BoxFit.cover,
+              ),
+            ),
+            SizedBox(height: 10),
+            Text(
+              '\$${loadedMovie.year}',
+              style: TextStyle(color: Colors.grey, fontSize: 20),
+            ),
+            SizedBox(height: 10),
+            Container(
+              width: double.infinity,
+              padding: EdgeInsets.all(10),
+              child: Text(
+                loadedMovie.title,
+                textAlign: TextAlign.center,
+                softWrap: true,
+              ),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
